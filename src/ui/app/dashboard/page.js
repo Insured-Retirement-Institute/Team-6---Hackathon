a3d6d2b345ddf15fbabaf3bf41a87c8924bf5382
ct-bess <bessellc@gmail.com>
2026-02-24 12:54:55 -0600

    updates dashboard and npm scripts

--- src/ui/app/dashboard/page.js
+++ src/ui/app/dashboard/page.js
@@ -3,6 +3,12 @@ export default function Dashboard() {
     <div>
       <main>
         <h1>This is where my dashboard will be</h1>
+        <div>
+          <span>Here we will display some basic cards with the logged in Agent. Just their name and such.</span>
+        </div>
+        <div>
+          <span>Down here will be a BoB style grid where we can interact with each contract/policy and open up a modal to submit a AOR change reassignment</span>
+        </div>
       </main>
     </div>
   );
